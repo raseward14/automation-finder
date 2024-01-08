f6201fe82eb1c1484ea5e4682ece471b915ec5f7
Peter Bishop <pjb.den@gmail.com>
2024-01-07 18:08:59 -0700

    oauth implemented and socket.io added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OAuthClickUp from "./components/oauth";
+import { io, Socket } from "socket.io-client";
+import { ClientToServerEvents, ServerToClientEvents } from "./models/socket";
 
-function App() {
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+  "http://localhost:3002"
+);
+
+const App: React.FC<{}> = () => {
+  // client-side
+  socket.on("connect", () => {
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/oauth" element={<OAuthClickUp />}></Route>
+        <Route path="/oauth/success" element={<OAuthClickUp />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
