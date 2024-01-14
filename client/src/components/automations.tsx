88e247b53911a4f0c88d04dd51b28abf4ddc6dde
richardseward <raseward14@gmail.com>
2024-01-14 12:12:58 -0700

    bearer token setSate works

--- client/src/components/automations.tsx
+++ client/src/components/automations.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import * as AutomationAPIFunctions from '../utils/AutomationsAPI';
 
-const Automations= () => {
-  const [bearer, setBearer] = useState('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InNiVkFxWkNGdVJBPSJ9.eyJ1c2VyIjoxNDkxNzI4NywidmFsaWRhdGVkIjp0cnVlLCJzZXNzaW9uX3Rva2VuIjp0cnVlLCJ3c19rZXkiOjI2MTQ1NTczOTMsImlhdCI6MTcwNTIwODYzMSwiZXhwIjoxNzA1MzgxNDMxfQ.9hnFChSCVfuVSLhxQUtpZrkTH1z_svLbGJMmjYfcPoU');
+const Automations = () => {
+  const [bearer, setBearer] = useState('');
   const [shard, setShard] = useState('prod-us-east-2-1');
 
   const [workspaceID, setWorkspaceID] = useState(10618731);
@@ -12,46 +12,63 @@ const Automations= () => {
 
   let triggerIDArray;
 
-  
-  
-  function printBearer(): void {
-    const triggerIDValue = document.getElementById('bearer-input') as HTMLInputElement;
-    const printValue = document.getElementById('enteredNumber') as HTMLOutputElement;
-    const trigID = triggerIDValue.value;
-    printValue.textContent = trigID.toString();
+  const getShard = async () => {
+    fetch(`https://app.clickup.com/shard/v1/handshake/${workspaceID}`, {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   };
 
+  function printBearer(): void {
+    const triggerIDValue = document.getElementById(
+      'bearer-input'
+    ) as HTMLInputElement;
+    const printValue = document.getElementById(
+      'enteredNumber'
+    ) as HTMLOutputElement;
+    const trigID = triggerIDValue.value;
+    printValue.textContent = trigID.toString();
+    setBearer(trigID);
+  }
+
   const getListAutomations = (listIDs: number[]) => {
-    listIDs.forEach(id => {
-      const workflowLog = AutomationAPIFunctions.getListAutomations(shard, id, bearer);
+    listIDs.forEach((id) => {
+      const workflowLog = AutomationAPIFunctions.getListAutomations(
+        shard,
+        id,
+        bearer
+      );
       console.log(`list ${id} workflow: ${workflowLog}`);
-    })
+    });
   };
 
   const getFolderAutomations = (folderIDs: number[]) => {
-    folderIDs.forEach(id => {
-      const workflowLog = AutomationAPIFunctions.getFolderAutomations(shard, id, bearer);
+    folderIDs.forEach((id) => {
+      const workflowLog = AutomationAPIFunctions.getFolderAutomations(
+        shard,
+        id,
+        bearer
+      );
       console.log(`folder ${id} workflow: ${workflowLog}`);
-    })
+    });
   };
 
   const getSpaceAutomations = (spaceIDs: number[]) => {
-    spaceIDs.forEach(id => {
-      const workflowLog = AutomationAPIFunctions.getSpaceAutomations(shard, id, bearer);
+    spaceIDs.forEach((id) => {
+      const workflowLog = AutomationAPIFunctions.getSpaceAutomations(
+        shard,
+        id,
+        bearer
+      );
       console.log(`space ${id} workflow: ${workflowLog}`);
-
-    } )
+    });
   };
 
-  // useEffect(() => {
-  //     async (req: Request, res: Response): Promise<any> => {
-  //       const request = await fetch(`https://app.clickup.com/shard/v1/handshake/${workspaceID}`, {
-  //         method: "GET",
-  //       })
-  //       const response = await request.text();
-  //       console.log('useEffect fired', response);
-  //     }
-  // },[]);
+  useEffect(() => {
+    console.log('useEffet fired');
+    getShard();
+  });
 
   // useEffect(() => {
   //   console.log(bearer)
@@ -60,18 +77,17 @@ const Automations= () => {
   return (
     <div>
       <h1>The automations page</h1>
-      <span>Enter a trigger ID</span><br/>
+      <span>Enter a trigger ID</span>
+      <br />
 
-      <input 
-      type= "text"
-      id='bearer-input'
-      placeholder="bearer"
-      /><br/>
-      <button
-      onClick={() => printBearer()}>Print</button>
-      <h3>You entered: <output id="enteredNumber"></output></h3>
+      <input type="text" id="bearer-input" placeholder="bearer" />
+      <br />
+      <button onClick={() => printBearer()}>Print</button>
+      <h3>
+        You entered: <output id="enteredNumber"></output>
+      </h3>
     </div>
-  )
+  );
 };
 
 export default Automations;
