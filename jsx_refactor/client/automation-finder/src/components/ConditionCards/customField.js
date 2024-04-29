e2c63a2e72aef6a390397387b36d03a268626635
richardseward <raseward14@gmail.com>
2024-04-28 21:37:31 -0600

    fixed broken relationship any task in workspace image

--- jsx_refactor/client/automation-finder/src/components/ConditionCards/customField.js
+++ jsx_refactor/client/automation-finder/src/components/ConditionCards/customField.js
@@ -4,7 +4,8 @@ import Card from 'react-bootstrap/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 import ProgressBar from 'react-bootstrap/ProgressBar';
-import "./style.css"
+import ClickUpIcon from '../images/cu-white.png';
+import './style.css';
 
 const CustomFieldCard = ({ cardDetails, key }) => {
   const [fieldId, setFieldId] = useState(cardDetails.name.slice(3));
@@ -14,15 +15,18 @@ const CustomFieldCard = ({ cardDetails, key }) => {
   const [JWT, setJWT] = useState(localStorage.getItem('jwt'));
 
   const getCustomField = async (id) => {
-    const res = await axios.post('http://localhost:8080/automation/customField', {
-      shard: cardDetails.shard,
-      fieldId: fieldId,
-      bearer: JWT
-    })
+    const res = await axios.post(
+      'http://localhost:8080/automation/customField',
+      {
+        shard: cardDetails.shard,
+        fieldId: fieldId,
+        bearer: JWT,
+      }
+    );
     if (res?.data) {
-      setCustomField(res.data)
+      setCustomField(res.data);
     }
-  }
+  };
 
   const renderIcon = (action) => {
     switch (action.type_id) {
@@ -32,158 +36,215 @@ const CustomFieldCard = ({ cardDetails, key }) => {
         if (action?.type_config?.ai) {
           // ai
           return (
-            <div className='condition-icon'>
-              <FontAwesomeIcon className='icon' icon={icon({ name: "wand-magic-sparkles" })} /><>{customField?.name}</>
+            <div className="condition-icon">
+              <FontAwesomeIcon
+                className="icon"
+                icon={icon({ name: 'wand-magic-sparkles' })}
+              />
+              <>{customField?.name}</>
             </div>
-          )
+          );
         } else {
           // text area
           return (
-            <div className='condition-icon'>
-              <FontAwesomeIcon className='icon' icon={icon({ name: "i-cursor" })} /><>{customField?.name}</>
+            <div className="condition-icon">
+              <FontAwesomeIcon
+                className="icon"
+                icon={icon({ name: 'i-cursor' })}
+              />
+              <>{customField?.name}</>
             </div>
-          )
-
+          );
         }
         break;
       case 15:
         // short text
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "t" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon className="icon" icon={icon({ name: 't' })} />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 2:
         // email
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "envelope" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon"
+              icon={icon({ name: 'envelope' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 7:
         // number
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "hashtag" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon"
+              icon={icon({ name: 'hashtag' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 0:
         // website
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "globe" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon className="icon" icon={icon({ name: 'globe' })} />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 3:
         // phone
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "phone-flip" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon"
+              icon={icon({ name: 'phone-flip' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 8:
         // currency
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "dollar-sign" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon"
+              icon={icon({ name: 'dollar-sign' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 6:
         // checkbox
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "square-check" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon"
+              icon={icon({ name: 'square-check' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 4:
-        // add a function to convert 1713520800000 to a date
+        // date
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "calendar" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon"
+              icon={icon({ name: 'calendar' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 16:
-        // add function to loop through attachment URLs and display them
+        // attachment
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "paperclip" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon"
+              icon={icon({ name: 'paperclip' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 12:
         //  Label Custom Field
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "tag" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon className="icon" icon={icon({ name: 'tag' })} />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 19:
-        //address
+        // address
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "location-dot" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon"
+              icon={icon({ name: 'location-dot' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 10:
         // users, we need to loop through userIds and print each user - array of numbers
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "user" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon className="icon" icon={icon({ name: 'user' })} />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 11:
         // this is rating, we should use the type_config?.count prop so we know the total, and then the cardDetails.value for the numeric value user has set
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "star" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon className="icon" icon={icon({ name: 'star' })} />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 14:
-        // manual progress 
+        // manual progress
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "bars-progress" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon"
+              icon={icon({ name: 'bars-progress' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 9:
         // this is a tasks relationship type_config does not have subcategory_id for any task in Workspace cardDetails.value is an array of task_id strings
         return (
-          <div className='condition-icon'>
-            <img src="../images/clickup.jpeg"></img><>{customField?.name}</>
+          <div className="condition-icon">
+            <img src={ClickUpIcon}></img>
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 18:
-        // relationship specific list 
+        // relationship specific list
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon list-relationship' icon={icon({ name: "arrow-right-arrow-left" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon list-relationship"
+              icon={icon({ name: 'arrow-right-arrow-left' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
+        );
         break;
       case 1:
         // dropdown
         return (
-          <div className='condition-icon'>
-            <FontAwesomeIcon className='icon' icon={icon({ name: "square-caret-down" })} /><>{customField?.name}</>
+          <div className="condition-icon">
+            <FontAwesomeIcon
+              className="icon"
+              icon={icon({ name: 'square-caret-down' })}
+            />
+            <>{customField?.name}</>
           </div>
-        )
-        break
+        );
+        break;
     }
-  }
+  };
 
   const getValue = (action) => {
     switch (action.type_id) {
@@ -202,15 +263,15 @@ const CustomFieldCard = ({ cardDetails, key }) => {
       // phone
       case 8:
         // currency
-        setValueText(cardDetails.value)
+        setValueText(cardDetails.value);
         break;
       case 6:
         // checkbox
         // insert logic to convert boolean to text
         if (valueText) {
-          setValueText("checked")
+          setValueText('checked');
         } else {
-          setValueText("unchecked")
+          setValueText('unchecked');
         }
         break;
       case 4:
@@ -224,60 +285,60 @@ const CustomFieldCard = ({ cardDetails, key }) => {
         // add function to loop through attachment URLs and display them
         let attachmentString = '';
         fieldValue.map((item, i) => {
-          if ((i + 1) === fieldValue.length) {
+          if (i + 1 === fieldValue.length) {
             // its the last item in the array, and does not need a comma
             let newString = attachmentString.concat(item);
             setValueText(newString);
           } else {
-            let newString = attachmentString.concat(item + ", ");
+            let newString = attachmentString.concat(item + ', ');
             attachmentString = newString;
           }
-        })
+        });
         break;
       case 12:
         // fieldValue is an array of labelIDs, we need to convert them to their txt value
         const labelValueArray = customField?.type_config?.options;
         let labelString = '';
         let labelTxtArray = fieldValue.map((value) => {
-          let found = labelValueArray.find((label) => value === label.id)
+          let found = labelValueArray.find((label) => value === label.id);
           return found.label;
-        })
+        });
 
         labelTxtArray.map((label, i) => {
-          if ((i + 1) === labelTxtArray.length) {
+          if (i + 1 === labelTxtArray.length) {
             let newString = labelString.concat(label);
             setValueText(newString);
           } else {
-            let newString = labelString.concat(label + ", ");
+            let newString = labelString.concat(label + ', ');
             labelString = newString;
           }
-        })
+        });
         break;
       case 19:
-        setValueText(cardDetails?.value?.formatted_address)
+        setValueText(cardDetails?.value?.formatted_address);
         break;
       case 10:
         // users, we need to loop through userIds and print each user - array of numbers
         let userIdString = '';
         fieldValue.map((item, i) => {
-          if ((i + 1) === fieldValue.length) {
+          if (i + 1 === fieldValue.length) {
             // its the last item in the array, and does not need a comma
             let newString = userIdString.concat(item);
             setValueText(newString);
           } else {
-            let newString = userIdString.concat(item + ", ");
+            let newString = userIdString.concat(item + ', ');
             userIdString = newString;
           }
-        })
+        });
         break;
       case 11:
         // this is rating, we should use the type_config?.count prop so we know the total, and then the cardDetails.value for the numeric value user has set
-        let ratioString = `${fieldValue}/${action?.type_config?.count}`
+        let ratioString = `${fieldValue}/${action?.type_config?.count}`;
         setValueText(ratioString);
         break;
       case 14:
         // this is an manual progress - total is in type_config?.end prop, and users value is in cardDetails.value.current - its a string
-        setValueText({ type: "manual-progress", value: fieldValue?.current })
+        setValueText({ type: 'manual-progress', value: fieldValue?.current });
         // setValueText(fieldValue?.current)
         break;
       case 18:
@@ -286,78 +347,81 @@ const CustomFieldCard = ({ cardDetails, key }) => {
         // this is a tasks relationship type_config does not have subcategory_id for any task in Workspace cardDetails.value is an array of task_id strings
         let taskIdString = '';
         fieldValue.map((item, i) => {
-          if ((i + 1) === fieldValue.length) {
+          if (i + 1 === fieldValue.length) {
             // its the last item in the array, and does not need a comma
             let newString = taskIdString.concat(item);
             setValueText(newString);
           } else {
-            let newString = taskIdString.concat(item + ", ");
+            let newString = taskIdString.concat(item + ', ');
             taskIdString = newString;
           }
-        })
+        });
         break;
       case 1:
         // dropdown
         let valueArray = customField?.type_config?.options;
-        let result = valueArray?.find(item => item.id === fieldValue);
+        let result = valueArray?.find((item) => item.id === fieldValue);
         setValueText(result?.name);
         break;
     }
-  }
+  };
 
   useEffect(() => {
-    console.log('from Custom Field card', customField)
+    console.log('from Custom Field card', customField);
     if (customField !== undefined) {
       // the only way to do this is to switch the field type_id prop
       // will need to call function from here
       // function can perform this find method
-      getValue(customField)
+      getValue(customField);
 
       // this logic works for dropdowns
       let valueArray = customField?.type_config?.options;
       console.log(valueArray);
-      console.log(fieldValue)
+      console.log(fieldValue);
       // let result = valueArray?.find(item => item.id === fieldValue);
       // setValueText(result?.name);
-
     }
-  }, [customField])
+  }, [customField]);
 
   useEffect(() => {
-    getCustomField(fieldId)
-  }, [fieldId])
+    getCustomField(fieldId);
+  }, [fieldId]);
 
   return (
     <>
       <Card className="condition-card" key={key}>
         <Card.Body>
-          <Card.Title className='value'>
-            {`Custom Field`}
-          </Card.Title>
+          <Card.Title className="value">{`Custom Field`}</Card.Title>
           <Card className="value">{cardDetails.op}</Card>
-          <span><b className='card-text'>FIELD</b></span>
+          <span>
+            <b className="card-text">FIELD</b>
+          </span>
           {customField && valueText ? (
             <>
-              <Card className='value'>
-                {renderIcon(customField)}
-              </Card>
-              <span><b className='card-text'>VALUE</b></span>
-              {valueText?.type === "manual-progress" ? (
+              <Card className="value">{renderIcon(customField)}</Card>
+              <span>
+                <b className="card-text">VALUE</b>
+              </span>
+              {valueText?.type === 'manual-progress' ? (
                 <>
-                  <ProgressBar variant="success" now={valueText?.value} label={`${valueText?.value}`} />
+                  <ProgressBar
+                    variant="success"
+                    now={valueText?.value}
+                    label={`${valueText?.value}`}
+                  />
                 </>
-
               ) : (
-
                 <>
-                  <Card className='value'>{valueText}</Card>
+                  <Card className="value">{valueText}</Card>
                 </>
               )}
             </>
-          ) : (<></>)}
+          ) : (
+            <></>
+          )}
         </Card.Body>
       </Card>
     </>
-  )
-}
+  );
+};
 export default CustomFieldCard;
