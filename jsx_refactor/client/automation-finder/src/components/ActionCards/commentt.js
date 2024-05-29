5802f3de79fb32a702a907f07a504d163eae46df
Richard Seward <richardseward@Richards-Laptop.local>
2024-05-29 13:20:14 -0600

    added commentArray map to comment.js file as a new approach, text renders but not styled and line breaks still not correct

--- jsx_refactor/client/automation-finder/src/components/ActionCards/commentt.js
+++ jsx_refactor/client/automation-finder/src/components/ActionCards/commentt.js
@@ -4,19 +4,20 @@ import "./style.css"
 
 const CommentCard = ({ cardDetails, key }) => {
     const [commentArray, setCommentArray] = useState(cardDetails?.action?.input?.comment);
+    const [commentText, setCommentText] = useState();
 
-    const testFunction = (obj, i) => {
-        let line = '';
-        if(obj.text === '\n') {
-            return <br/>;
-        } else {
-            line = line.concat(obj.text);
-            return <span>{line}</span>
-        }
-    }
-
+    const CommentText = (props) => {
+        console.log(props.text);
+        const text = props.text;
+        const newText = text.split('\n').map(str => <p className='text'>{str.text}</p>);
+        return newText;
+   }
     useEffect(() => {
         console.log('comment array: ', commentArray);
+        if(commentArray) {
+            let text = commentArray.map(obj => obj.text);
+            setCommentText(text)
+        }
     }, [commentArray])
     return (
         <>
@@ -25,17 +26,7 @@ const CommentCard = ({ cardDetails, key }) => {
                     <Card.Title className='value'>
                         {cardDetails.name}
                     </Card.Title>
-                    <Card className='status'>{commentArray ? (
-                    <>
-                        {commentArray.map((text, i) => (
-                            // <span>{text.text}</span>
-                            testFunction(text)
-                        ))}
-                    </>
-                    ) : (
-                        <></>
-                    )
-                    }</Card>
+                    <Card className='status'><CommentText text={`${commentText}`}/></Card>
                 </Card.Body>
             </Card>
         </>
