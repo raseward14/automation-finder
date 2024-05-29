10e2df2bd0315a4ff318afe591fedc6a928776ad
Richard Seward <richardseward@Richards-Laptop.local>
2024-05-28 20:43:00 -0600

    updated comment card import so the right file is imported

--- jsx_refactor/client/automation-finder/src/components/ActionCards/comment.js
+++ jsx_refactor/client/automation-finder/src/components/ActionCards/comment.js
@@ -3,6 +3,10 @@ import Card from 'react-bootstrap/Card';
 import "./style.css"
 
 const CommentCard = ({ cardDetails, key }) => {
+    
+    useEffect(() => {
+        console.log('another testt', cardDetails?.action?.input?.comment);
+    })
     return (
         <>
             <Card className="action-card" key={key}>
