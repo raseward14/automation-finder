14bc9dffac8e7bd3593e36d79d15f44c6b0e2776
Richard Seward <richardseward@Richards-Laptop.local>
2024-05-28 21:26:09 -0600

    comment card mvp with commas is rendering, no styles yet

--- jsx_refactor/client/automation-finder/src/components/ActionCards/comment.js
+++ jsx_refactor/client/automation-finder/src/components/ActionCards/comment.js
@@ -3,10 +3,22 @@ import Card from 'react-bootstrap/Card';
 import "./style.css"
 
 const CommentCard = ({ cardDetails, key }) => {
-    
+    const [commentArray, setCommentArray] = useState(cardDetails?.action?.input?.comment);
+    const [commentText, setCommentText] = useState();
+
+    const CommentText = (props) => {
+        console.log(props.text);
+        const text = props.text;
+        const newText = text.split('\n').map(str => <p className='text'>{str.text}</p>);
+        return newText;
+   }
     useEffect(() => {
-        console.log('another testt', cardDetails?.action?.input?.comment);
-    })
+        console.log('comment array: ', commentArray);
+        if(commentArray) {
+            let text = commentArray.map(obj => obj.text);
+            setCommentText(text)
+        }
+    }, [commentArray])
     return (
         <>
             <Card className="action-card" key={key}>
@@ -14,6 +26,7 @@ const CommentCard = ({ cardDetails, key }) => {
                     <Card.Title className='value'>
                         {cardDetails.name}
                     </Card.Title>
+                    <Card className='status'><CommentText text={`${commentText}`}/></Card>
                 </Card.Body>
             </Card>
         </>
