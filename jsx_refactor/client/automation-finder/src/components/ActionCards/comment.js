e56fc747d5285708f68910063ae0d427f23eab5f
Richard Seward <richardseward@Richards-Laptop.local>
2024-06-03 22:48:59 -0600

    appending classlists to div elements on line break - text content not showing yet

--- jsx_refactor/client/automation-finder/src/components/ActionCards/comment.js
+++ jsx_refactor/client/automation-finder/src/components/ActionCards/comment.js
@@ -6,28 +6,35 @@ const CommentCard = ({ cardDetails, key }) => {
     const [commentArray, setCommentArray] = useState(cardDetails?.action?.input?.comment);
     let line = '';
     let lineObjects = [];
-
+    
     const testFunction = (obj, i) => {
         if (obj.text === '\n') {
+            // create new var, and set old to empty for new line
             let newLine = line;
-            let newArray = lineObjects
             line = '';
-            lineObjects = []
-            console.log(newArray);
-            // return <div>
-            //     {newArray.forEach((item) => {
-            //         if ("bold" in item.attributes) {
-            //             <strong>{item.text}</strong>
-            //         } else {
-            //             <div>{item.text}</div>
-            //         }
 
-            //     })}
-            // </div>
+            // create a new array for our line objects, and empty global array for nxt line
+            let newArray = lineObjects;
+            console.log('array of objects for this line', newArray)
+            lineObjects = [];
+            // create a new span element, and append all divs in array
+            let newSpan = document.createElement('span');
+            lineObjects.forEach((element) => {
+                console.log(element)
+                newSpan.appendChild(element)
+            })
+            console.log('the new line', newSpan);
+
+            // print the new line to the page
             return <span>{newLine}</span>
+            // return {newSpan};
         } else {
             line = line.concat(obj.text);
-            lineObjects.push(obj);
+            let newDiv = document.createElement('div');
+            newDiv.innerHTML = `${obj.text}`;
+            let attributeArray = Object.keys(obj?.attributes);
+            attributeArray.forEach(key => newDiv.classList.add(`${key}`));
+            lineObjects.push(newDiv);
         }
     }
 
