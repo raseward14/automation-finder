13853e1b7794de53b2d81a75a2b1b922aabf6d30
Richard Seward <richardseward@Richards-Laptop.local>
2024-06-10 18:32:30 -0600

    writing a new function using state vars

--- jsx_refactor/client/automation-finder/src/components/ActionCards/comment.js
+++ jsx_refactor/client/automation-finder/src/components/ActionCards/comment.js
@@ -4,44 +4,61 @@ import "./style.css"
 
 const CommentCard = ({ cardDetails, key }) => {
     const [commentArray, setCommentArray] = useState(cardDetails?.action?.input?.comment);
+    const [textArray, setTextArray] = useState([]);
     let line = '';
-    let lineObjects = [];
-    let attributeString = '';
-    
-    const testFunction = (obj, i) => {
-        if (obj.text === '\n') {
-            // create new var, and set old to empty for new line
-            let newLine = line;
-            line = '';
+    let lineArray = [];
+    let container = document.getElementById('line-container');
 
-            // create a new array for our line objects, and empty global array for nxt line
-            let newArray = lineObjects;
-            console.log('array of objects for this line', newArray)
-            lineObjects = [];
-            // print the new line to the page
-            // return <span>{newLine}</span>
+    const test2Function = (obj, i) => {
+        console.log('text object: ', obj)
+        if (obj.text === '\n') {
+            setTextArray(lineArray);
+            console.log('complete line: ', textArray)
+            lineArray = [];
         } else {
-            attributeString = '';
-            line = line.concat(obj.text);
-            let attributeArray = Object.keys(obj?.attributes);
-            attributeArray.forEach(style =>  {
-                console.log('single attribute', style)
-                let newString = attributeString.concat(style);
-                let addSpace = newString.concat(' ');
-                attributeString = addSpace;
-            })
-            console.log(attributeString);
-            let newDiv = document.createElement('div');
-            let content = document.createTextNode(`${obj.text}`);
-            newDiv.appendChild(content);
-            if(attributeString !== '') {
-                newDiv.classList.add(attributeString);
+            lineArray.push(obj)
+        }
+
+    }
+
+    useEffect(() => {
+        if(textArray.length > 0) {
+            console.log('the current line is: ', textArray)
+        }
+    }, [textArray])
+
+    const testFunction = (obj, i) => {
+        console.log('text object: ', obj)
+        if (obj.text === '\n') {
+            console.log('one complete line: ', lineArray);
+            // create a new line
+            let newSpan = document.createElement('span');
+            newSpan.classList.add('inline');
+            // array of text objects to print on this line
+            let newArr = lineArray;
+            // set global var back to empty
+            lineArray = [];
+            newArr.map((obj, i) => {
+                // for each text object, create a new div
+                let newDiv = document.createElement('div');
+                // give the div text
+                let content = document.createTextNode(`${obj.text}`);
+                newDiv.appendChild(content);
+                // create an array of its attributes, and append them to the new element
+                let attributeArray = Object.keys(obj?.attributes);
+                attributeArray.forEach(style =>  {
+                    newDiv.classList.add(style);
+                });
+                // now append that styled div, with text to the line container
+                newSpan.appendChild(newDiv); 
+            });
+            // and append that line to the comment field
+            console.log('container: ', container);
+            if((container !== null) && (newSpan !== null)) {
+                container.appendChild(newSpan);
             }
-            let container = document.getElementById('container-test');
-            if(newDiv !== null) {
-                container.appendChild(newDiv);
-            }
-            // return <div className={attributeString}>{`${obj.text}`}</div>
+        } else {
+            lineArray.push(obj);
         }
     }
 
@@ -55,17 +72,22 @@ const CommentCard = ({ cardDetails, key }) => {
                     <Card.Title className='value'>
                         {cardDetails.name}
                     </Card.Title>
-                    <Card className='status'>{commentArray ? (
+                    <Card className='status'>
+                    <div id='line-container'></div>
+                    {commentArray ? (
                         <>
-                            {commentArray.map((text, i) => (
-                                testFunction(text)
-                            ))}
-                            <span id='container-test'></span>
+                            {
+                                commentArray.map((text, i) => (
+                                    // testFunction(text)
+                                    test2Function(text)
+                                ))
+                            }
                         </>
                     ) : (
                         <></>
                     )
-                    }</Card>
+                    }
+                    </Card>
                 </Card.Body>
             </Card>
         </>
