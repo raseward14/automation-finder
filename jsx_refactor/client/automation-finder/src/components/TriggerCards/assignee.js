94c557f825d7601a8eb11b3a966953e0ac8495f9
Richard Seward <richardseward@Richards-Laptop.local>
2024-06-17 18:36:47 -0600

    looking for endppoint to return Workspace users

--- jsx_refactor/client/automation-finder/src/components/TriggerCards/assignee.js
+++ jsx_refactor/client/automation-finder/src/components/TriggerCards/assignee.js
@@ -2,12 +2,28 @@ import { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import "./style.css";
 
-// assignee added
-// assignee removed
+// if cardDetails.type === 'assignee'
+// assignee added > cardDetails.trigger.conditions[0].after forEach (array)
+// if cardDetails.type === 'assignee_removed'
+// assignee removed > cardDetails.trigger.conditions[0].before forEach (array)
 const AssigneeCard = ({ triggerName, cardDetails }) => {
+    const [assigneeArray, setAssigneeArray] = useState([]);
+
+    useEffect(() => {
+        if(assigneeArray.length) {
+            console.log('assignees are: ', assigneeArray)
+        }
+    }, [assigneeArray]);
+
     useEffect(() => {
         console.log('assignee card details: ', cardDetails)
+        if(cardDetails.type === 'assignee') {
+            setAssigneeArray(cardDetails.trigger.conditions[0].after);
+        } else {
+            setAssigneeArray(cardDetails.trigger.conditions[0].before);
+        };
     }, [cardDetails])
+
     return (
         <>
             <Card>
