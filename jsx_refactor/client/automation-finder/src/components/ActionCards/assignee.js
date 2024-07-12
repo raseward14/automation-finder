ba9b4924db5e7b69230c58300ac88367f5dddcd4
Richard Seward <richardseward@Richards-Laptop.local>
2024-07-11 18:06:51 -0600

    ouline for assignee action

--- jsx_refactor/client/automation-finder/src/components/ActionCards/assignee.js
+++ jsx_refactor/client/automation-finder/src/components/ActionCards/assignee.js
@@ -3,6 +3,19 @@ import Card from 'react-bootstrap/Card';
 import "./style.css"
 
 const AssigneeCard = ({ cardDetails, key}) => {
+  // Remove all assignees green checkbox
+    // action.input.unassign: true
+  // if this is true, nothing else is rendered
+  // if this is not true, the unassign property doesn't exist in the input object
+
+  // Add assignees (default) (single avatar array, but spread across two properties) - handled the same way as assignee added/removed triggers
+    // action.input.add_assignees: [14917287]
+    // action.input.add_special_assignees: ['watchers', 'creator', 'triggered_by']
+  // Remove assignees (single avatar array, but spread across two properties) - handled the same way as assignee added/removed triggers
+    // action.input.rem_assignees: [14917287]
+    // action.input.rem_special_assignees: ['watchers', 'creator', 'triggered_by', 'assignees'] -> there is one additional option here - assignees
+  // Reassign
+    // action.input.assignees: [14917287, 61313973, 37710627] -> no dynamic options available here
   useEffect(() => {
     // console.log('this is a testt')
   })
